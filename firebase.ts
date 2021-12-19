06fdfb7ff320f89e84240658c510f81d0456fb8a
joaomanaia <joaopfmanaia@gmail.com>
2021-12-19 20:25:58 +0000

    remove emulator

--- firebase.ts
+++ firebase.ts
@@ -41,8 +41,8 @@ if (!appArray.length) {
 const auth = getAuth()
 const firestore = getFirestore()
 
-connectAuthEmulator(auth, 'http://localhost:9099')
-connectFirestoreEmulator(firestore, 'localhost', 8080)
+//connectAuthEmulator(auth, 'http://localhost:9099')
+//connectFirestoreEmulator(firestore, 'localhost', 8080)
 
 export { app, auth, firestore }
 
