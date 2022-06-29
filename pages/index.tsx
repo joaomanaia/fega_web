3434ce36623c6e3416cea6af886ad1400ae6d605
João Manaia <53819675+joaomanaia@users.noreply.github.com>
2022-06-29 11:27:01 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -38,7 +38,7 @@ const Home: NextPage = () => {
       <Script
         async
         crossOrigin="anonymous"
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1923025671607389"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5667420475984673"
       />
 
       <div
