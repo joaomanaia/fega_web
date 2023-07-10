187ff73977bf4cd3fba7cc2ecd8143a4cb7bea79
joaomanaia <joaopfmanaia@gmail.com>
2023-07-10 23:41:56 +0100

    update page

--- app/(core)/cameras/[id]/page.tsx
+++ app/(core)/cameras/[id]/page.tsx
@@ -4,27 +4,27 @@ import MainContainer from "@/components/m3/MainContainer"
 import CameraType from "@/types/CameraType"
 import { Metadata } from "next"
 
-const getCameraById = async (id: string) => {
-  const res = await import("@/app/api/cameras/route")
-  const cameras: CameraType[] = await (await res.GET()).json()
+const getCameraById = async (id: string): Promise<CameraType | null> => {
+  const api = await import("@/app/api/cameras/route")
 
-  return cameras.find((camera) => camera.id === id) || null
+  return api
+    .GET()
+    .then((res) => res.json() as Promise<CameraType[]>)
+    .then((cameras) => cameras.find((camera) => camera.id === id) ?? null)
 }
 
 type CameraPageProps = {
   params: { id: string }
 }
 
-export async function generateMetadata(
-  { params }: CameraPageProps
-): Promise<Metadata> {
+export async function generateMetadata({ params }: CameraPageProps): Promise<Metadata> {
   const id = params.id
 
   const camera = await getCameraById(id)
 
   return {
     title: camera?.name ?? "Camera not found",
-    description: camera?.description ?? "", 
+    description: camera?.description ?? "",
   }
 }
 
