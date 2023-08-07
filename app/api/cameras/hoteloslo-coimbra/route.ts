b09171914f38aa384adb20190314a2ea317edbe8
joaomanaia <joaopfmanaia@gmail.com>
2023-08-07 10:35:36 +0100

    Update hosteloslo camera api

--- app/api/cameras/hoteloslo-coimbra/route.ts
+++ app/api/cameras/hoteloslo-coimbra/route.ts
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic"
+export const revalidate = 0
+
 export async function GET() {
   const cameraBaseUrl = "http://hoteloslo-coimbra.dnsalias.com:50000/SnapshotJPEG"
   const cameraUrl = `${cameraBaseUrl}?date=${Date.now()}`
