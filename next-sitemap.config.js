248262e319380f40d1eefe020df61ae07a541f3f
joaomanaia <joaopfmanaia@gmail.com>
2023-08-10 11:37:02 +0100

    remove unused deps

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next-sitemap').IConfig} */
-
 module.exports = {
-  siteUrl: "https://www.fega.ml",
+  siteUrl: process.env.SITE_URL || "https://fega.vercel.app/en",
   generateRobotsTxt: true,
-};
+}
