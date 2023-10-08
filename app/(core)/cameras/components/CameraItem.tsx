35fe6e245457fb4abf0b87002a07e61689b5e79b
joaomanaia <joaopfmanaia@gmail.com>
2023-10-08 11:33:37 +0100

    Make camera item selectable

--- app/(core)/cameras/components/CameraItem.tsx
+++ app/(core)/cameras/components/CameraItem.tsx
@@ -4,15 +4,19 @@ import CameraType from "@/types/CameraType"
 import { Avatar, ListItemAvatar, ListItemButton, ListItemText } from "@mui/material"
 import Image from "next/image"
 import Link from "next/link"
+import { useParams } from "next/navigation"
 
 interface CameraItemProps {
   camera: CameraType
 }
 
 const CameraItem: React.FC<CameraItemProps> = ({ camera }) => {
+  const params = useParams()
+  const selected = params.id === camera.id
+
   return (
-    <Link className="h-fit text-inherit bg-inherit decoration-transparent" href={`/cameras/${camera.id}`}>
-      <ListItemButton>
+    <Link className="h-fit next-link" href={`/cameras/${camera.id}`}>
+      <ListItemButton selected={selected}>
         <ListItemAvatar>
             <Avatar>
                 <Image src={camera.image_poster} alt={camera.name} width={40} height={40} />
