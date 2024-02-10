f1abceb509ac38b979e46115d67751c30084cd2f
joaomanaia <joaopfmanaia@gmail.com>
2024-02-10 19:44:22 +0000

    Fix auth layout height

--- app/(login)/auth/components/LoginContainer.tsx
+++ app/(login)/auth/components/LoginContainer.tsx
@@ -5,7 +5,7 @@ interface LoginContainerProps {
 export const LoginContainer: React.FC<LoginContainerProps> = ({ children }) => {
   return (
     <div className="h-screen w-screen bg-primary text-primary-foreground flex flex-col lg:flex-row items-center justify-center">
-      <div className="h-fit lg:h-full w-full lg:w-1/2 flex flex-col items-center justify-center">
+      <div className="grow lg:h-full w-full lg:w-1/2 flex flex-col items-center justify-center">
         <h2 className="text-3xl lg:text-4xl xl:text-6xl">Welcome to Fega</h2>
 
         <p className="text-lg lg:text-xl mt-0">Best social network in ega!</p>
