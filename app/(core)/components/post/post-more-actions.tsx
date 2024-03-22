cc5125f558249224b030d9095cb7e91160ca81ce
joaomanaia <joaopfmanaia@gmail.com>
2024-03-22 14:05:34 +0000

    Remove unused import and update styles in post-more-actions.tsx and dropdown-menu.tsx

--- app/(core)/components/post/post-more-actions.tsx
+++ app/(core)/components/post/post-more-actions.tsx
@@ -7,7 +7,6 @@ import {
 } from "@/components/ui/dropdown-menu"
 import { deletePost } from "@/core/actions/postActions"
 import { MoreHorizontalIcon } from "lucide-react"
-import { revalidatePath } from "next/cache"
 import { toast } from "sonner"
 
 interface PostMoreActionsProps {
