fc1a3efed7f25edc91d0eeace8e2bf01b06cf42d
joaomanaia <joaopfmanaia@gmail.com>
2024-05-04 12:06:50 +0100

    refactor: Update i18n-config to use TypeScript const assertions for locales

--- i18n-config.ts
+++ i18n-config.ts
@@ -1,9 +1,14 @@
-export const i18n = {
-  defaultLocale: "en",
-  locales: ["en", "pt"],
-} as const
+const locales = ["en", "pt"] as const
 
-export type Locale = (typeof i18n.locales)[number]
+export type Locale = (typeof locales)[number]
+
+export const i18n: {
+  defaultLocale: Locale
+  locales: typeof locales
+} = {
+  defaultLocale: "en",
+  locales: locales,
+} as const
 
 export type Language = {
   locale: Locale
