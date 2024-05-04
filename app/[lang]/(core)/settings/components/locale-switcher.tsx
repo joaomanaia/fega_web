c75644509caad206b3e49e3a66da52f59be25e34
joaomanaia <joaopfmanaia@gmail.com>
2024-05-04 22:27:59 +0100

    Fix locale switcher button not refreshing the page

--- app/[lang]/(core)/settings/components/locale-switcher.tsx
+++ app/[lang]/(core)/settings/components/locale-switcher.tsx
@@ -26,29 +26,11 @@ export const LocaleSwitcher: React.FC<LocaleSwitcherProps> = ({ currentLocale, d
   const pathName = usePathname()
 
   const redirectedPathName = (locale: Locale) => {
-    if (!pathName) return "/"
-
-    const pathnameIsMissingLocale = i18n.locales.every(
-      (locale) => !pathName.startsWith(`/${locale}/`) && pathName !== `/${locale}`
-    )
-
-    if (pathnameIsMissingLocale) {
-      if (locale === i18n.defaultLocale) return pathName
-      return `/${locale}${pathName}`
-    } else {
-      if (locale === i18n.defaultLocale) {
-        const segments = pathName.split("/")
-        const isHome = segments.length === 2
-        if (isHome) return "/"
-
-        segments.splice(1, 1)
-        return segments.join("/")
-      }
-
-      const segments = pathName.split("/")
-      segments[1] = locale
-      return segments.join("/")
+    if (pathName.startsWith(`/${currentLocale}/`)) {
+      return pathName.replace(`/${currentLocale}/`, `/${locale}/`)
     }
+
+    return pathName
   }
 
   return (
