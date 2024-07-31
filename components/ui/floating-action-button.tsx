a6d33d60f44fcd08f257a6e95094d0d9f0158fd0
joaomanaia <joaopfmanaia@gmail.com>
2024-07-31 18:39:32 +0100

    Added disabled color to FAB

--- components/ui/floating-action-button.tsx
+++ components/ui/floating-action-button.tsx
@@ -12,7 +12,7 @@ export const ExtendedFAB = React.forwardRef<HTMLButtonElement, ExtendedFABProps>
     return (
       <Comp
         className={cn(
-          "h-16 text-xl px-6 rounded-[25px] bg-primaryContainer hover:bg-primaryContainer/80 text-primaryContainer-foreground inline-flex border-none cursor-pointer items-center justify-center whitespace-nowrap font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none",
+          "h-16 text-xl px-6 rounded-[25px] bg-primaryContainer disabled:bg-surfaceVariant/[0.28] hover:bg-primaryContainer/80 text-primaryContainer-foreground disabled:text-surfaceVariant inline-flex border-none cursor-pointer items-center justify-center whitespace-nowrap font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none",
           className
         )}
         ref={ref}
