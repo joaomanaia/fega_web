0d2598e1b5df21116a662c88e8c079292c3137d7
joaomanaia <joaopfmanaia@gmail.com>
2024-08-01 17:30:43 +0100

    Updated the main page loading content

--- app/[lang]/(core)/loading.tsx
+++ app/[lang]/(core)/loading.tsx
@@ -1,3 +1,18 @@
+import { CreatePostSkeleton } from "@/app/components/create_post/CreatePost"
+import { MainContainer } from "@/app/components/m3/main-container"
+import { PostSkeleton } from "@/app/components/post/Post"
+
 export default function Loading() {
-    return <div>Loading posts...</div>
-  }
+  return (
+    <main className="flex flex-col md:pb-3 md:gap-4 lg:flex-row-reverse w-full h-full overflow-auto lg:overflow-hidden">
+      <CreatePostSkeleton />
+      <MainContainer className="flex flex-col rounded-none md:rounded-[30px] space-y-4 md:space-y-6 lg:w-full lg:overflow-auto">
+        <PostSkeleton />
+        <PostSkeleton />
+        <PostSkeleton />
+        <PostSkeleton />
+        <PostSkeleton />
+      </MainContainer>
+    </main>
+  )
+}
--- app/components/create_post/CreatePost.tsx
+++ app/components/create_post/CreatePost.tsx
@@ -7,6 +7,7 @@ import { MainContainer } from "../m3/main-container"
 import { cn } from "@/lib/utils"
 import { Input } from "@/components/ui/input"
 import { type Dictionary } from "@/get-dictionary"
+import { Skeleton } from "@/components/ui/skeleton"
 
 type CreatePostTypes = {
   className?: string
@@ -47,3 +48,15 @@ const CreatePost: React.FC<CreatePostTypes> = ({ className, dictionary }) => {
 }
 
 export default CreatePost
+
+export const CreatePostSkeleton = () => {
+  return (
+    <MainContainer className="h-fit rounded-b-none md:rounded-[30px]">
+      <div className="flex flex-col gap-4 lg:min-w-52 xl:min-w-64">
+        <Skeleton className="h-8 w-full" />
+        <Skeleton className="h-8 w-full" />
+        <Skeleton className="h-8 w-full" />
+      </div>
+    </MainContainer>
+  )
+}
