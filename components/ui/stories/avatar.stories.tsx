06dd5cd4274bad42ace40b75ce3698e7eb5f2068
joaomanaia <joaopfmanaia@gmail.com>
2024-08-15 22:02:17 +0100

    chore: Update dependencies and add Storybook addon for accessibility

---
+++ components/ui/stories/avatar.stories.tsx
@@ -0,0 +1,40 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { Avatar, AvatarFallback, AvatarImage } from "../avatar"
+
+const meta = {
+  title: "Components/ui/Avatar",
+  render: (args) => (
+    <Avatar>
+      <AvatarImage src="/fega_round.svg" alt="fega" />
+      <AvatarFallback>F</AvatarFallback>
+    </Avatar>
+  ),
+  tags: ["autodocs"],
+  parameters: {
+    layout: "centered",
+  },
+} satisfies Meta<typeof Avatar>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const AvatarWithImage: Story = {
+  args: {},
+  render: (args) => (
+    <Avatar>
+      <AvatarImage src="/fega_round.svg" alt="@fega" />
+      <AvatarFallback>F</AvatarFallback>
+    </Avatar>
+  ),
+}
+export const AvatarWithFallback: Story = {
+  args: {},
+  render: (args) => (
+    <Avatar>
+      <AvatarImage src="" alt="@fega" />
+      <AvatarFallback>F</AvatarFallback>
+    </Avatar>
+  ),
+}
