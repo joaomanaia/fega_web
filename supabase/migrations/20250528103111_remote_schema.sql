a35ee93723bd58175408b99288611ee0e20f8087
joaomanaia <joaopfmanaia@gmail.com>
2025-05-28 11:40:31 +0100

    Update supabase version

---
+++ supabase/migrations/20250528103111_remote_schema.sql
@@ -0,0 +1,29 @@
+revoke delete on table "public"."user_roles" from "anon";
+
+revoke insert on table "public"."user_roles" from "anon";
+
+revoke references on table "public"."user_roles" from "anon";
+
+revoke select on table "public"."user_roles" from "anon";
+
+revoke trigger on table "public"."user_roles" from "anon";
+
+revoke truncate on table "public"."user_roles" from "anon";
+
+revoke update on table "public"."user_roles" from "anon";
+
+revoke delete on table "public"."user_roles" from "authenticated";
+
+revoke insert on table "public"."user_roles" from "authenticated";
+
+revoke references on table "public"."user_roles" from "authenticated";
+
+revoke select on table "public"."user_roles" from "authenticated";
+
+revoke trigger on table "public"."user_roles" from "authenticated";
+
+revoke truncate on table "public"."user_roles" from "authenticated";
+
+revoke update on table "public"."user_roles" from "authenticated";
+
+
