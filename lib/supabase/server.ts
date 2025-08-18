fa1cedfaee8547e09b1a27534d8aa79347f1924b
joaomanaia <joaopfmanaia@gmail.com>
2025-08-18 22:18:11 +0100

    Add types to supabase server client

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -3,24 +3,29 @@ import { cache } from "react"
 import { cookies } from "next/headers"
 import { createServerClient } from "@supabase/ssr"
 import { env } from "@/env"
+import type { Database } from "@/types/database.types"
 
 export const createClient = cache(async () => {
   const cookieStore = await cookies()
 
-  return createServerClient(env.NEXT_PUBLIC_SUPABASE_URL, env.NEXT_PUBLIC_SUPABASE_ANON_KEY, {
-    cookies: {
-      getAll() {
-        return cookieStore.getAll()
+  return createServerClient<Database>(
+    env.NEXT_PUBLIC_SUPABASE_URL,
+    env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) => {
+              cookieStore.set(name, value, options)
+            })
+          } catch (error) {
+            console.error("Failed to set cookies", error)
+          }
+        },
       },
-      setAll(cookiesToSet) {
-        try {
-          cookiesToSet.forEach(({ name, value, options }) => {
-            cookieStore.set(name, value, options)
-          })
-        } catch (error) {
-          console.error("Failed to set cookies", error)
-        }
-      },
-    },
-  })
+    }
+  )
 })
